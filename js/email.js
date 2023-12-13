d071d5023d7bd16fa7ad857aca52d986c971e9fd
Jericho Lardizabal <jericholardizabal2002@gmail.com>
2023-12-13 22:46:46 +0800

    updated

--- js/email.js
+++ js/email.js
@@ -1,16 +1,17 @@
 function sendMail() {
-    var params = {
-        name: document.getElementById("name").value,
-        email: document.getElementById("email").value,
-        subject: document.getElementById("subject").value,
-        message: document.getElementById("message").value,
-    };
+    // Get input values
+    var name = document.getElementById("name").value;
+    var email = document.getElementById("email").value;
+    var subject = document.getElementById("subject").value;
+    var message = document.getElementById("message").value;
 
-    if (name === "" || email === "" || subject === "" || message === "") {
-        alert("Please fill out all required fields.");
+    // Validate inputs
+    if (!name || !email || !subject || !message) {
+        alert("Please fill in all fields");
         return;
     }
 
+    // Create parameters object
     var params = {
         name: name,
         email: email,
@@ -18,16 +19,26 @@ function sendMail() {
         message: message,
     };
 
+    // EmailJS configuration
+    const serviceId = "service_c88wscn";
+    const templateId = "template_z0t74km";
 
-const serviceId = "service_c88wscn"
-const templateId = "template_z0t74km"
-emailjs.send(serviceId, templateId, params).then((res) => {
-        document.getElementById("name").value = "",
-            document.getElementById("email").value = "",
-            document.getElementById("subject").value = "",
-            document.getElementById("message").value = "",
+    // Send email using EmailJS
+    emailjs.send(serviceId, templateId, params)
+        .then((res) => {
+            // Clear form fields
+            document.getElementById("name").value = "";
+            document.getElementById("email").value = "";
+            document.getElementById("subject").value = "";
+            document.getElementById("message").value = "";
+
+            // Log and alert success
             console.log(res);
-        alert("Your Message sent successfully!");
-    })
-    .catch((err) => console.log(err));
+            alert("Your Message sent successfully!");
+        })
+        .catch((err) => {
+            // Log and alert error
+            console.log(err);
+            alert("An error occurred while sending the message. Please try again later.");
+        });
 }
